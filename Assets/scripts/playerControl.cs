94f5aada36a24a8bf447ff345b3bd839ca8cfd8f
DamienBen <damien.benloukil@gmail.com>
2014-01-27 21:03:00 +0100

    cleaning code

--- Assets/scripts/playerControl.cs
+++ Assets/scripts/playerControl.cs
@@ -14,45 +14,30 @@ public class playerControl : MonoBehaviour
 	private		PlayerState _playerState;
 	private 	Vector3 	_moveDirection = Vector3.zero;
 	private		float		_speed = 5.0F;
-	private		float		_tempPos = 2.0F;
-	private		Vector3 v = new Vector3(2, 0, 0);
+	private		float		_playerDestination = 0.0F;
+	private		Vector3		_playerMove = Vector3.zero;
+
 
 	void Start () 
 	{
 		_playerState = PlayerState.right;
+		_playerMove = new Vector3(20, 0, 0);
 	}
 	
 
 	void Update () 
 	{
 		CharacterController controller = GetComponent<CharacterController>();
-		_moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		_moveDirection = transform.TransformDirection(_moveDirection);
-		_moveDirection *= _speed;
-
 	
-
-
-		//Debug.Log(transform.position.x);
-	//	float x = Input.GetAxis("Horizontal") * Time.smoothDeltaTime * speed;
-	//	float y = Input.GetAxis("Vertical") * Time.smoothDeltaTime * speed;
-	//	transform.Translate(x,0,0/*y*/,/*Space.Self*/0);
-	
-
 		if (Input.GetKeyDown ("left"))
 		{
 			if (_playerState != PlayerState.left)
 			{
 				transform.Rotate (0, 180, 0);
-				v.x *= -1;
+				_playerMove.x *= -1;
 			}
 			_playerState = PlayerState.left;
-			_tempPos -= 2;
-
-
-
-		//	_moveDirection.x -=  100;
-		//	transform.Translate(2, 0, 0, Space.Self);
+			_playerDestination -= 1;
 
 		}
 		if (Input.GetKeyDown ("right"))
@@ -60,28 +45,26 @@ public class playerControl : MonoBehaviour
 			if (_playerState != PlayerState.right)
 			{
 				transform.Rotate (0, 180, 0);
-				v.x *= -1;
+				_playerMove.x *= -1;
 			}
 			_playerState = PlayerState.right;
-			_tempPos += 2;
+			_playerDestination += 1;
 
-
-		//	_moveDirection.x +=  100;
-		//	transform.Translate(2, 0, 0, Space.Self);	
 		}
 
 
-		if (_playerState == PlayerState.right) {
-						if (transform.position.x <= _tempPos)
-								controller.Move (v * Time.deltaTime);
+		if (_playerState == PlayerState.right) 
+		{
+			if (transform.position.x < _playerDestination)
+				controller.Move (_playerMove * Time.deltaTime);
 		}
 		else
 		{
-			if (transform.position.x >= _tempPos)
-				controller.Move (v * Time.deltaTime);
+			if (transform.position.x >= _playerDestination)
+				controller.Move (_playerMove * Time.deltaTime);
 		}
 
 
-		print (transform.position.x + "/" + _tempPos + "/" + v.x );
+		print (transform.position.x + "/" + _playerDestination + "/" + _playerMove.x );
 	}
 }
