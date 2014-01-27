91d8441753ac28c6aa0b8a01e65c8ac2f1b2cc91
Damien <damien.benloukil@izitaxi.fr>
2014-01-27 18:10:35 +0100

    Char move mechanic done need to refactoring

--- Assets/scripts/playerControl.cs
+++ Assets/scripts/playerControl.cs
@@ -14,6 +14,8 @@ public class playerControl : MonoBehaviour
 	private		PlayerState _playerState;
 	private 	Vector3 	_moveDirection = Vector3.zero;
 	private		float		_speed = 5.0F;
+	private		float		_tempPos = 2.0F;
+	private		Vector3 v = new Vector3(2, 0, 0);
 
 	void Start () 
 	{
@@ -26,10 +28,11 @@ public class playerControl : MonoBehaviour
 		CharacterController controller = GetComponent<CharacterController>();
 		_moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		_moveDirection = transform.TransformDirection(_moveDirection);
-		//Multiply it by speed.
 		_moveDirection *= _speed;
 
 	
+
+
 		//Debug.Log(transform.position.x);
 	//	float x = Input.GetAxis("Horizontal") * Time.smoothDeltaTime * speed;
 	//	float y = Input.GetAxis("Vertical") * Time.smoothDeltaTime * speed;
@@ -39,25 +42,46 @@ public class playerControl : MonoBehaviour
 		if (Input.GetKeyDown ("left"))
 		{
 			if (_playerState != PlayerState.left)
+			{
 				transform.Rotate (0, 180, 0);
+				v.x *= -1;
+			}
 			_playerState = PlayerState.left;
-			_moveDirection.x -=  100;
+			_tempPos -= 2;
 
+
+
+		//	_moveDirection.x -=  100;
 		//	transform.Translate(2, 0, 0, Space.Self);
 
 		}
 		if (Input.GetKeyDown ("right"))
 		{
 			if (_playerState != PlayerState.right)
+			{
 				transform.Rotate (0, 180, 0);
+				v.x *= -1;
+			}
 			_playerState = PlayerState.right;
-			_moveDirection.x +=  100;
+			_tempPos += 2;
 
+
+		//	_moveDirection.x +=  100;
 		//	transform.Translate(2, 0, 0, Space.Self);	
 		}
 
-		controller.Move(_moveDirection * Time.deltaTime);
 
-		//print (_lfSpeed + "/" + d.x);
+		if (_playerState == PlayerState.right) {
+						if (transform.position.x <= _tempPos)
+								controller.Move (v * Time.deltaTime);
+		}
+		else
+		{
+			if (transform.position.x >= _tempPos)
+				controller.Move (v * Time.deltaTime);
+		}
+
+
+		print (transform.position.x + "/" + _tempPos + "/" + v.x );
 	}
 }
