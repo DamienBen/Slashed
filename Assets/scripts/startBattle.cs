b2c84146368c6538edc2173622e286cd12c8ef13
DamienBen <damien.benloukil@gmail.com>
2014-01-30 00:15:04 +0100

    add bg need to see infinite

--- Assets/scripts/startBattle.cs
+++ Assets/scripts/startBattle.cs
@@ -6,20 +6,40 @@ public class startBattle : MonoBehaviour
 
 	public 	GameObject vilain;
 	public 	GameObject background;
+	public 	GameObject backgroundLeft;
+	public 	GameObject backgroundRight;
+	public	float		backgroundWidth;
 	public float speed = 10f;
 
 
 	void Start ()
 	{
 		StartCoroutine (VilainSpawn());
+		backgroundWidth = background.renderer.bounds.size.x;
+		Vector3 v = new Vector3 (background.transform.position.x +backgroundWidth, background.transform.position.y, background.transform.position.z);
+		Vector3 v2 = new Vector3 (background.transform.position.x - backgroundWidth, background.transform.position.y, background.transform.position.z);
+		backgroundRight = (GameObject)Instantiate (background, v, Quaternion.identity);
+		backgroundLeft = (GameObject)Instantiate(background, v2, Quaternion.identity);
 	}
 
 	void Update()
 	{
 		GameObject p = GameObject.Find ("player");
 		transform.position = new Vector3 (p.transform.position.x, transform.position.y, transform.position.z);
-		//Instantiate(background, v2, Quaternion.identity);
 
+
+	
+		if (transform.position.x > 0) 
+		{
+			if (transform.position.x > backgroundRight.transform.position.x)
+				backgroundLeft.transform.position = new Vector3(background.transform.position.x + backgroundWidth * 2,backgroundRight.transform.position.y );
+		}
+		else
+		{
+			if (transform.position.x < backgroundLeft.transform.position.x)
+				backgroundRight.transform.position = new Vector3(background.transform.position.x - backgroundWidth * 2,backgroundLeft.transform.position.y );
+
+		}
 	}
 
 
@@ -40,7 +60,7 @@ public class startBattle : MonoBehaviour
 			}
 
 			Vector3 v = new Vector3(sSide,0,0);
-			Vector3 v2 = new Vector3(-20, 3, 0);
+		
 
 			Instantiate (vilain, v, Quaternion.identity); 
 
