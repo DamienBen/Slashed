353ba65e2a4c2657edc03b67ae4a1a9257bf2d76
DamienBen <damien.benloukil@gmail.com>
2014-01-29 21:41:36 +0100

    adding prefab and infinite bg (commented)

--- Assets/scripts/startBattle.cs
+++ Assets/scripts/startBattle.cs
@@ -4,27 +4,48 @@ using System.Collections;
 public class startBattle : MonoBehaviour 
 {
 
-	public GameObject vilain;
+	public 	GameObject vilain;
+	public 	GameObject background;
+	public float speed = 10f;
+
 
 	void Start ()
 	{
 		StartCoroutine (VilainSpawn());
 	}
 
+	void Update()
+	{
+		GameObject p = GameObject.Find ("player");
+		transform.position = new Vector3 (p.transform.position.x, transform.position.y, transform.position.z);
+		//Instantiate(background, v2, Quaternion.identity);
+
+	}
 
 
 	IEnumerator VilainSpawn()
 	{
 		while(true)
 		{
-
 			int side = Random.Range(1,200) % 2;
-			float sSide = -10;
+			float sSide = transform.position.x + 20.0F;
+
 			if (side == 1)
-				sSide = 10;
+			{
+
+				if (transform.position.x < 0)
+					sSide = transform.position.x - 20; 
+				else
+					sSide *= -1;
+			}
+
 			Vector3 v = new Vector3(sSide,0,0);
+			Vector3 v2 = new Vector3(-20, 3, 0);
+
 			Instantiate (vilain, v, Quaternion.identity); 
-			yield return new WaitForSeconds(1.1f);
+
+			
+			yield return new WaitForSeconds(4.6f);
 
 		}
 	}
